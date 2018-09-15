0ab842d01bf60668aa47f8357f68a407020c9594
gogakoreli <gogakoreli@gmail.com>
2018-09-15 16:29:18 +0400

    dashboard: add component #14

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, WelcomeComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    DashboardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
