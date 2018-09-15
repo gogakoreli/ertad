def75bbd4d4efc9d4858d7d8caf378a9b5e88c20
gogakoreli <gogakoreli@gmail.com>
2018-09-15 23:20:28 +0400

    api url from environment variable

--- front/src/app/api/api.service.ts
+++ front/src/app/api/api.service.ts
@@ -1,32 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Action } from './types';
 
-const apiUrl = "http://localhost:3500/";
+const apiUrl = environment.apiUrl;
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
 
   listRooms() {
-    return this.http.get(apiUrl + "rooms");
+    return this.http.get(apiUrl + 'rooms');
   }
 
   getRoomById(id: string) {
-    return this.http.get(apiUrl + "rooms/" + id);
+    return this.http.get(apiUrl + 'rooms/' + id);
   }
 
   listUsers() {
-    return this.http.get(apiUrl + "users");
+    return this.http.get(apiUrl + 'users');
   }
 
   addAction(action: Action) {
-    return this.http.post("/action", action);
+    return this.http.post(apiUrl + 'action', action);
   }
 
   getUserById(id: string) {
-    return this.http.get(apiUrl + "users/" + id);
+    return this.http.get(apiUrl + 'users/' + id);
   }
 }
