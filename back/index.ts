a65a850fe9c9ec4fdf02937b23aaae8603c79013
Sandro Dolidze <me@sdolidze.ge>
2018-09-15 19:35:31 +0400

    Fix socket.io

--- back/index.ts
+++ back/index.ts
@@ -1,13 +1,11 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import http from 'http';
 import sio from 'socket.io';
 import { actions$, add } from './src/domain';
 
 const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3500;
-const io = sio(new http.Server(app));
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -16,19 +14,21 @@ app.get('/', (_, res) => {
   res.json({ text: 'Hello World' });
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-});
-
 app.post('/action', (_, res) => {
   const action = res.body;
   add(action);
 });
 
-actions$.subscribe(action => {
-  io.emit('action', action);
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
 });
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/`);
+const io = sio.listen(server, { origins: '*:*' });
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
+
+actions$.subscribe(action => {
+  io.emit('action', action);
 });
