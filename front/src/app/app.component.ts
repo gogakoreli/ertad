4a66f387f743597f95ac9081b5c97452be5fee79
Sandro Dolidze <me@sdolidze.ge>
2018-09-15 23:56:45 +0400

    log all actions

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RealtimeService } from './api/realtime.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'front';
+  title = "front";
+  constructor(realtime: RealtimeService) {}
 }
